d26193da29d04c15b4876295ff881420ffdfc01d
Yung-Jer <wongyungjer29@gmail.com>
2022-03-04 20:25:01 +0800

    updated router setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import About from '@/views/About.vue'
+import HelloWorld from '@/views/HelloWorld.vue'
 
 const routes = [
+    {
+        path: '/',
+        name: 'Hello World',
+        component: HelloWorld
+    },
     {
         path: '/about',
         name: 'About',
