44b757a08d3d3413b31a300eff8b8a12cd2c8769
rein <tairaomisu@gmail.com>
2022-03-17 01:01:21 +0800

    completed delete reminders function

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="header">My Reminders</div>
+    <div id="header"><b>My Reminders</b></div>
     <table id="table">
     <tr>
     <th>Medication</th>
@@ -15,15 +15,13 @@
 
 <script>
 import firebaseApp from "../firebase.js";
-import { getDocs, getFirestore } from '@firebase/firestore';
+import { getDocs, getFirestore, deleteDoc } from '@firebase/firestore';
 import { doc, setDoc, collection } from "firebase/firestore"; //collection, getDoc
 const db = getFirestore(firebaseApp)
 
 export default {
   name: "Table",
-  beforeMount() {
-    this.showReminder()
-  },
+
   mounted() {
     async function display() {
         let z = await getDocs(collection(db, "Reminders"));
@@ -38,21 +36,50 @@ export default {
           var medDosage = (yy.Dosage);
           var cell1 = row.insertCell(0); var cell2 = row.insertCell(1); var cell3 = row.insertCell(2);
           var cell4 = row.insertCell(3);
-          // console.log(cell1.type);
+
           cell1.innerHTML = medName;
           cell2.innerHTML = medFunction;
           cell3.innerHTML = medDosage;
+
           //creating checkbox
           var checkBox = document.createElement("input");
           checkBox.type = "checkbox";
           checkBox.id = "checkBox";
           cell4.appendChild(checkBox);
 
+          //creating delete button for each reminder
+          var cell5 = row.insertCell(4);
+          var deleteButton = document.createElement("button");
+          deleteButton.id = "deleteButton";
+          deleteButton.innerHTML = "Delete";
+          deleteButton.onclick = function(){
+                confirmDelete(String(medName))
+            }
+          cell5.appendChild(deleteButton);
+
           index += 1;
         })
     }
     display()
-    
+    this.showReminder()
+
+    async function delReminder(reminder) {
+      await deleteDoc(doc(db, "Reminders", reminder))
+      console.log("deleted ", reminder);
+      let table = document.getElementById("table");
+      while (table.rows.length > 1){
+        table.deleteRow(1)
+      }
+      display()
+    }
+
+    async function confirmDelete(reminder) {
+      let isConfirmed = confirm("Delete this reminder?")
+      if (isConfirmed) {
+        delReminder(reminder)
+      }
+    }
+
   },
   methods: {
     showReminder() {
@@ -84,16 +111,7 @@ export default {
       cell1.appendChild(medName);
       cell2.appendChild(medFunction);
       cell3.appendChild(medDosage);
-      // cell4.appendChild(checkBox);
 
-      //creating the save button
-      // var saveButton = document.createElement("button");
-      // saveButton.textContent = "save";
-      
-      // saveButton.onclick = function() {
-      //   savetofs()
-      // }
-      // cell5.appendChild(saveButton);
     },
     async savetofs() {
     var medDosage=document.getElementById("meddosage").value;
@@ -111,6 +129,7 @@ export default {
       catch(error) {
         console.error("Error adding document: ", error);
       }
+      location.reload();
     }
   } 
 }
